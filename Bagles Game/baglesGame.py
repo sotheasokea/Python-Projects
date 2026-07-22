8c9ac1b11767ee814939b4a12d067c70205dd791
sotheasokea <sotheasokea7@gmail.com>
2026-07-22 18:46:15 +0700

    edit format and comments

--- Bagles Game/baglesGame.py
+++ Bagles Game/baglesGame.py
@@ -1,9 +1,8 @@
 import random
 
-
+#a normal instruction printed function
 def instruction():
     print("Bagels, a deductive logic game.")
-    print("Recoded by Sothea Sokea")
     print("\nI am Thinking of a 4-digit positive number. Try to guess what it is.")
     print("Here are some clues:")
     print("\nWhen I say: That means:")
@@ -13,10 +12,12 @@ def instruction():
     print("\nI have thought up a number.")
     print("-> You have 15 guesses to get it.\n")
 
+#it's better to : secretNumber = f"{random.randint(0, 9999):04d}"
 def getRandomNumber():
     number = random.randint(0, 9999)
     return f"{number:04d}"
 
+#printing the clue for each guess
 def getClue(guessNumber, secretNumber):
     if guessNumber == secretNumber:
         return "You got it!";
@@ -32,11 +33,14 @@ def getClue(guessNumber, secretNumber):
         clues.sort()
         return ' '.join(clues)
 
+#output the instruction of the game
 instruction()
 
 secretNumber = getRandomNumber()
-maxGuesses = 1
+maxGuesses = 1      # maxGuesses=10, it starts at 1
 result = "Try Again"
+
+#looping when player guessing the number
 while True:
     userGuess = input(f"Guess #{maxGuesses}: ")
     if len(userGuess) < 4:
@@ -52,6 +56,7 @@ while True:
         result = "Try Again"
         break;
 
+#show the answer if player can't guess correctly
 if result == "Try Again":
     print("\nThe secret number was:", secretNumber,"☠️\n")
 
